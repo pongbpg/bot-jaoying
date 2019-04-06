f800a63b8317facf42733de6da58d3d199ef34da
tawatchai.w <tawatchai.w@nextcorp.co.th>
2019-04-06 16:12:04 +0700

    live component

--- src/components/live/ShowPage.js
+++ src/components/live/ShowPage.js
@@ -30,48 +30,39 @@ export class LiveShowPage extends React.Component {
     }
     render() {
         return (
-            <div className="hero is-danger is-fullheight">
+            <div className="hero is-link is-fullheight">
                 <div className="hero-head" style={{ marginTop: '50px' }}>
                     <div className="container">
                         <div className="columns">
-                            <div className="column is-6">
-                                <h1 className="title is-1 has-text-white">ร้านเจ้าหญิง SHOP!</h1>
+                            <div className="column is-8">
+                                <h1 className="title has-text-white" style={{ fontSize: 150 }}>รหัส {this.state.live.id}</h1>
                             </div>
-                            <div className="column is-6 has-text-right" style={{ paddingTop: '25px' }}>
+                            <div className="column is-4 has-text-right" style={{ paddingTop: '25px' }}>
                                 <h1 className="title has-text-white is-2">
                                     วันที่ {moment(this.state.date).format('LL')}
                                     <br />เวลา {moment(this.state.date).format('LTS')}
                                 </h1>
                             </div>
+                        </div>
+                        <div className="columns" style={{ paddingTop: '30px' }}>
+                            <div className="column is-full">
+                                <h1 className="title is-2 has-text-white" style={{ fontSize: 130 }}>{this.state.live.name}</h1>
                             </div>
                         </div>
-                    </div>
-                    <div className="hero-body" style={{ paddingTop: '-250px' }}>
-                        <div className="container">
-                            <div className="columns">
-                                <div className="column is-full">
-                                    <h1 className="title is-1 has-text-white" style={{ fontSize: 80 }}>รหัส {this.state.live.id}</h1>
-                                </div>
-                            </div>
-                            <div className="columns" style={{ paddingTop: '70px' }}>
-                                <div className="column is-full">
-                                    <h1 className="title is-2 has-text-white" style={{ fontSize: 70 }}>{this.state.live.name}</h1>
-                                </div>
-                            </div>
-                            <div className="columns" style={{ marginTop: '70px' }}>
-                                <div className="column is-full">
-                                    <h1 className="title is-2 has-text-white" style={{ fontSize: 80 }}>ราคา {Money(this.state.live.price, 0)} บาท</h1>
-                                </div>
+                        <div className="columns" style={{ marginTop: '30px' }}>
+                            <div className="column is-full">
+                                <h1 className="title is-2 has-text-white" style={{ fontSize: 150 }}>ราคา {Money(this.state.live.price, 0)} บาท</h1>
                             </div>
                         </div>
                     </div>
                 </div>
-                )
-            }
-        }
+            </div>
+        )
+    }
+}
 const mapStateToProps = (state, props) => ({
-                    live: state.live
-            });
+    live: state.live
+});
 const mapDispatchToProps = (dispatch, props) => ({
-                });
+});
 export default connect(mapStateToProps, mapDispatchToProps)(LiveShowPage);
