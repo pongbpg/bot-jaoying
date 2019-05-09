e04fc0b333b142d97abb8d320664148f8abc14ab
tawatchai.w <tawatchai.w@nextcorp.co.th>
2019-05-09 12:28:39 +0700

    dailyBank

--- src/components/ReportPage.js
+++ src/components/ReportPage.js
@@ -184,6 +184,26 @@ export class ReportPage extends React.Component {
                                         </div>
                                     </td>
                                 </tr>
+                                < tr >
+                                    <td className="has-text-centered">5</td>
+                                    <td className="has-text-centered">ยอดโอนรวมแต่ละธนาคาร (วันที่เริ่ม-ถึงวันที่)</td>
+                                    <td className="has-text-centered">
+                                        <div className="field is-grouped is-grouped-centered">
+                                            <p className="control">
+                                                <a className="button is-danger is-centered is-small"
+                                                    href={`http://yaumjai.com:3000/api/jaoying/dailyBank?uid=${this.state.uid}&startDate=${moment(this.state.startDate).format('YYYY-MM-DD')}&endDate=${moment(this.state.endDate).format('YYYY-MM-DD')}&file=pdf&sum=all`}
+                                                    target="_blank">
+                                                    PDF</a>
+                                            </p>
+                                            <p className="control">
+                                                <a className="button is-success is-centered is-small"
+                                                    href={`http://yaumjai.com:3000/api/jaoying/dailyBank?uid=${this.state.uid}&startDate=${moment(this.state.startDate).format('YYYY-MM-DD')}&endDate=${moment(this.state.endDate).format('YYYY-MM-DD')}&file=excel&sum=all`}
+                                                    target="_blank">
+                                                    EXCEL</a>
+                                            </p>
+                                        </div>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
 
